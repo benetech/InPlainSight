ccdbff047112a764bad35cebc61ea6b7e616ca48
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2016-06-08 13:56:59 -0700

    Initial Chrome Extension

---
+++ extension/popup.js
@@ -0,0 +1,102 @@
+// Initialise stego.
+var models = {};
+var stego = new MarkovTextStego();
+var codec = new stego.Codec(null);
+// Focus on input textarea.
+$('#input-text').focus();
+// When corpus selector changes:
+$('#corpus-selector').change(function () {
+  if ($('#corpus-selector').val() === 'custom') {
+    $('#input-corpus').attr('disabled', false).focus();
+  } else {
+    $('#input-corpus').attr('disabled', true);
+  }
+});
+// Disable encode/decode button when there is nothing to process.
+$('#input-text').bind('input propertychange', function (e) {
+  if ($('#input-text').val()) {
+    $('#encode-text').button('reset');
+  } else {
+    $('#encode-text').attr('disabled', true);
+  }
+});
+$('#output-text').bind('input propertychange', function (e) {
+  if ($('#output-text').val()) {
+    $('#decode-text').button('reset');
+  } else {
+    $('#decode-text').attr('disabled', true);
+  }
+});
+// When "Encode Text" button is clicked:
+$('#encode-text').click(function () {
+  // Change button state.
+  $('#encode-text').button('loading');
+  $('#decode-text').attr('disabled', false);
+  // Create or set ngramModel.
+  var corpusOption = $('#corpus-selector').val();
+  if (corpusOption === 'custom') {
+    if (models.hasOwnProperty(corpusOption)) {
+      delete models[corpusOption];
+    }
+    models[corpusOption] = new stego.NGramModel(2);
+    try {
+      models[corpusOption].import([$('#input-corpus').val()]);
+    } catch (e) {
+      if (e instanceof stego.NGramModelException) {
+        alert('Invalid corpus specified. See limitations.\n\n' +
+              'Error message: ' + e.message);
+        // Reset button state.
+        $('#decode-text').text('Decode Text');
+        $('#encode-text').button('reset');
+        return;
+      }
+    }
+  } else {
+    if (!models.hasOwnProperty(corpusOption)) {
+      models[corpusOption] = new stego.NGramModel(2);
+      models[corpusOption].import(corpora[corpusOption]);
+    }
+  }
+  codec.setModel(models[corpusOption]);
+  // Encode text.
+  try {
+    $('#output-text').val(codec.encode($('#input-text').val()));
+  } catch (e) {
+    if (e instanceof stego.CodecException) {
+      alert('Could not encode data.\n\n' +
+            'Error message: ' + e.message);
+      // Reset button state.
+      $('#encode-text').text('Encode Text');
+      $('#decode-text').button('reset');
+      return;
+    }
+  }
+  // Reset input text.
+  $('#input-text').val('');
+  // Reset button state.
+  $('#encode-text').text('Encode Text');
+  $('#decode-text').button('reset');
+});
+// When "Decode Text" button is clicked:
+$('#decode-text').click(function () {
+  // Change button state.
+  $('#decode-text').button('loading');
+  // Decode text.
+  try {
+    $('#input-text').val(codec.decode($.trim($('#output-text').val())));
+  } catch (e) {
+    if (e instanceof stego.CodecException) {
+      alert('Could not decode text.\n\n' +
+            'Error message: ' + e.message);
+      // Reset button state.
+      $('#encode-text').text('Encode Text');
+      $('#decode-text').button('reset');
+      return;
+    }
+  }
+  // Reset output text.
+  $('#output-text').val('');
+  // Reset button state.
+  $('#decode-text').text('Decode Text');
+  $('#encode-text').button('reset');
+});
