e9c6bd9509e0accd58d16bff3cf5d762983fa4b8
John Higgins <johnh@Benetech.local>
2016-06-14 14:57:58 -0700

    update source processing to work for more books and unicode

--- script/source_text_preprocessor.py
+++ script/source_text_preprocessor.py
@@ -1,17 +1,8 @@
 # I suggest we remove importance.txt because it is not general enough. 
 
-import os
+import os, re, codecs
 
-def should_filter_text(str):
-  if not str or str.startswith(" "):
-    return True
-  if str.startswith("Chapter") or str.startswith("CHAPTER"):
-    return True
-  if not any(c.islower() for c in str):
-    return True
-  return False
-
-f = open('../markovTextStego.js/demo/js/corpora_new.js', 'w')
+f = codecs.open('../extension/js/corpora.js', 'w',"utf-8")
 # Write the header
 output = """/**
  * Test corpora for markovTextStego.js.
@@ -22,29 +13,33 @@ output = """/**
 corpora = {
 """
 
-for file in os.listdir('../corpus'):
+for file in os.listdir('../corpus2'):
   filename = file[:file.find(".txt")]
   output += "'" + filename + "': JSON.parse('["
-  with open('../corpus/' + file, 'r') as text_file:
+  with codecs.open('../corpus2/' + file, 'r',"utf-8") as text_file:
       text = text_file.read()
-  start = text.find("*** START OF THIS PROJECT GUTENBERG")
-  end1 = text.find("End of Project Gutenberg")
-  end2 = text.find("*** END OF THE PROJECT")
-  end = 0
-  if end1 < 0:
-    end = end2
-  elif end2 < 0:
-    end = end1
-  else:
-    end = min(end1, end2)
 
-  paragraphs = text[start:end].split("\n\n")
+  startmatch = re.search(r'\*\*\*\s?START OF (THIS|THE) PROJECT GUTENBERG.*?\*\*\*',text)
+  start = startmatch.end()
+
+  endmatch = re.search(r'\*\*\*\s?END OF (THIS|THE) PROJECT',text)
+  end = endmatch.start()
+
+  print(filename)
+  print(start)
+  print(end)
+    
+  paragraphs = text[start:end].splitlines()
+  
   for paragraph in paragraphs:
-    paragraph = paragraph.strip()
-    if not should_filter_text(paragraph):
-      output += ('"' + paragraph.replace('\n', ' ') + '", ')
-  output = output[:len(output)-2]
-  output += "]'),\n"
+    if paragraph != "":
+      paragraph = paragraph.strip()
+      output += ('"' + paragraph.replace('\n', ' ').replace("'","\\'").replace('"',"\\'").replace(":","\\:").replace(";","\\;") + '", ')
+      output = output[:len(output)-1]
 
+  output = output[:len(output)-1]
+  output += "]'),\n"
+  
 output += "};\n"
-f.write(output)
+f.write(output)  
+f.close()
